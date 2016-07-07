2f7f75cb5ae47388f13d3a338d86097249d34959
lucaszw <lucas.zeer@gmail.com>
2016-07-06 19:08:34 -0700

    Added A Tab for Pretrained Models.
    Can be used for Fine Tuning.
    
    fixed build problem
    
    upload pretrained models for torch
    
    allowed uploading pretrained models with torch
    
    updated PretrainedModel.js

---
+++ digits/static/js/components/PretrainedModel.js
@@ -0,0 +1,90 @@
+// d3 alert menu for uploading a pretrained model:
+// Call renderer to draw component:
+
+var PretrainedModel = function(props){
+
+  var self = this;
+  var inputs = PretrainedModel.mixins;
+
+  self.container = d3.select(props.selector);
+  self.innerContainer = null;
+  self.frameworkSelector = null;
+
+  self.frameworkChanged = function(){
+    var nextFramework = self.frameworkSelector.property("value");
+    if (nextFramework ==  "torch"){
+      self.renderTorchForm();
+    }else{
+      self.renderCaffeForm();
+    }
+  };
+
+  self.render = function(){
+    self.container.html('');
+    var row = self.container.append("div").attr("class","row");
+    inputs.field(row.append("div").attr("class","col-xs-6"),"text","Jobname", "job_name");
+
+    self.frameworkSelector = inputs.select(
+      row.append("div").attr("class","col-xs-6"),["caffe", "torch"],"Framework","framework"
+    );
+    self.frameworkSelector.on("change", self.frameworkChanged);
+    self.innerContainer = self.container.append("div");
+    self.renderCaffeForm();
+  };
+
+  self.renderCaffeForm = function(){
+    self.innerContainer.html('');
+
+    inputs.field(self.innerContainer,"file","Weights (**.caffemodel)", "weights_file");
+    inputs.field(self.innerContainer,"file","Model Definition: (original.prototxt)", "model_def_file");
+    inputs.field(self.innerContainer,"file","Labels file: (Optional)", "labels_file");
+
+    self.innerContainer.append("button").attr({type: "submit",class: "btn btn-default"})
+      .on("click",self.submit)
+      .style("background","white")
+      .html("Upload Model");
+  };
+
+  self.renderTorchForm = function(e){
+    self.innerContainer.html('');
+
+    inputs.field(self.innerContainer,"file","Weights (**.t7)", "weights_file");
+    inputs.field(self.innerContainer,"file","Model Definition: (model.lua)", "model_def_file");
+    inputs.field(self.innerContainer,"file","Labels file: (Optional)", "labels_file");
+
+    self.innerContainer.append("button").attr({type: "submit",class: "btn btn-default"})
+      .on("click",self.submit)
+      .style("background","white")
+      .html("Upload Model");
+  };
+
+};
+
+PretrainedModel.mixins = {
+  select: function(obj,data,label,name){
+    var group = obj.append("div").attr("class","form-group");
+    group.append("label")
+        .attr("for",name).html(label);
+
+    var mySelect = group.append("select").attr({
+        class: "form-control",
+        name: name
+      });
+
+    mySelect.selectAll('option').data(data).enter()
+      .append("option")
+        .attr("value",function(d){return d})
+        .text(function(d){return d});
+
+    return mySelect;
+
+  },
+  field: function(obj,type,label,name){
+    var group = obj.append("div").attr("class","form-group");
+    group.append("label")
+        .attr("for",name).html(label);
+    group.append("input")
+      .attr({type: type, class: "form-control", name: name});
+  }
+
+};
