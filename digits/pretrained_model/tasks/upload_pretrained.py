67ecfaa20a6c49fa594af11759fe1f5038d0bd2b
lucaszw <lucas.zeer@gmail.com>
2016-07-06 19:08:34 -0700

    Convert Training Jobs to Pretrained Model Jobs

---
+++ digits/pretrained_model/tasks/upload_pretrained.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+from __future__ import absolute_import
+
+import digits
+from digits.task import Task
+from digits.utils import subclass, override
+import subprocess
+
+@subclass
+class UploadPretrainedModelTask(Task):
+    """
+    A task for uploading pretrained models
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Arguments:
+        weights_path -- path to model weights (**.caffemodel or ***.t7)
+        model_def_path  -- path to model definition (**.prototxt or ***.lua)
+        image_info -- a dictionary containing image_type, resize_mode, width, and height
+        labels_path -- path to text file containing list of labels
+        framework  -- framework of this job (ie caffe or torch)
+        """
+        self.weights_path = kwargs.pop('weights_path', None)
+        self.model_def_path = kwargs.pop('model_def_path', None)
+        self.image_info = kwargs.pop('image_info', None)
+        self.labels_path = kwargs.pop('labels_path', None)
+        self.framework = kwargs.pop('framework', None)
+
+        # resources
+        self.gpu = None
+
+        super(UploadPretrainedModelTask, self).__init__(**kwargs)
+
+    @override
+    def name(self):
+        return 'Upload Pretrained Model'
+
+    @override
+    def __setstate__(self, state):
+        super(UploadPretrainedModelTask, self).__setstate__(state)
+
+    @override
+    def process_output(self, line):
+        return True
+
+    @override
+    def offer_resources(self, resources):
+        reserved_resources = {}
+        # we need one CPU resource from inference_task_pool
+        cpu_key = 'inference_task_pool'
+        if cpu_key not in resources:
+            return None
+        for resource in resources[cpu_key]:
+            if resource.remaining() >= 1:
+                reserved_resources[cpu_key] = [(resource.identifier, 1)]
+                # we reserve the first available GPU, if there are any
+                gpu_key = 'gpus'
+                if resources[gpu_key]:
+                    for resource in resources[gpu_key]:
+                        if resource.remaining() >= 1:
+                            self.gpu = int(resource.identifier)
+                            reserved_resources[gpu_key] = [(resource.identifier, 1)]
+                            break
+                return reserved_resources
+        return None
+
+    def get_model_def_path(self):
+        """
+        Get path to model definition
+        """
+        raise NotImplementedError('Please implement me')
+
+    def get_weights_path(self):
+        """
+        Get path to model weights
+        """
+        raise NotImplementedError('Please implement me')
+
+    def move_file(self,input, output,env):
+        args  = ["cp", input, self.job_dir+"/"+output]
+        p = subprocess.Popen(args,env=env)
---
+++ digits/static/js/components/PretrainedModel.js
@@ -0,0 +1,128 @@
+// d3 alert menu for uploading a pretrained model:
+// Call renderer to draw component:
+
+var PretrainedModel = function(props){
+
+  var self = this;
+  var inputs = PretrainedModel.mixins;
+
+  self.container = d3.select(props.selector);
+  self.innerContainer = null;
+  self.frameworkSelector = null;
+  self.frameworks = [
+    {text: "Caffe", value: "caffe"},
+    {text: "Torch", value: "torch"}
+  ];
+  self.resize_channels = [
+    {text: "Color", value: 3},
+    {text: "Grayscale", value: 1}
+  ];
+
+  self.resize_modes = [
+    {text:"Squash", value: "squash"},
+    {text: "Crop", value: "crop"},
+    {text: "Fill", value: "fill"},
+    {text: "Half Crop, Half Fill", value: "half_crop"}
+  ];
+
+  self.frameworkChanged = function(){
+    var nextFramework = self.frameworkSelector.property("value");
+    if (nextFramework ==  "torch"){
+      self.renderTorchForm();
+    }else{
+      self.renderCaffeForm();
+    }
+  };
+  self.newRow = function(){
+    return self.container.append("div").attr("class","row");
+  };
+
+  self.render = function(){
+    self.container.html('');
+    var row = self.newRow();
+    inputs.field(row.append("div").attr("class","col-xs-6"),"text","Jobname", "job_name");
+
+    self.frameworkSelector = inputs.select(
+      row.append("div").attr("class","col-xs-6"),self.frameworks,"Framework","framework"
+    );
+
+    row = self.newRow();
+    row.style("background", "whitesmoke");
+    row.style("border-radius", "5px 5px 0px 0px");
+    inputs.select(
+      row.append("div").attr("class","col-xs-6"),
+        self.resize_channels,"Image Type","image_type"
+    );
+
+    inputs.select(
+      row.append("div").attr("class","col-xs-6"),
+        self.resize_modes,"Resize Mode","resize_mode"
+    );
+
+    row = self.newRow();
+    row.style("background", "whitesmoke");
+    row.style("border-radius", "0px 0px 5px 5px");
+    inputs.field(row.append("div").attr("class","col-xs-6"),"number","Width", "width").attr("value",256);
+    inputs.field(row.append("div").attr("class","col-xs-6"),"number","Height", "height").attr("value",256);
+
+    self.frameworkSelector.on("change", self.frameworkChanged);
+    self.innerContainer = self.container.append("div");
+    self.renderCaffeForm();
+  };
+
+  self.renderCaffeForm = function(){
+    self.innerContainer.html('');
+
+    inputs.field(self.innerContainer,"file","Weights (**.caffemodel)", "weights_file");
+    inputs.field(self.innerContainer,"file","Model Definition: (original.prototxt)", "model_def_file");
+    inputs.field(self.innerContainer,"file","Labels file: (Optional)", "labels_file");
+
+    self.innerContainer.append("button").attr({type: "submit",class: "btn btn-default"})
+      .on("click",self.submit)
+      .style("background","white")
+      .html("Upload Model");
+  };
+
+  self.renderTorchForm = function(e){
+    self.innerContainer.html('');
+
+    inputs.field(self.innerContainer,"file","Weights (**.t7)", "weights_file");
+    inputs.field(self.innerContainer,"file","Model Definition: (model.lua)", "model_def_file");
+    inputs.field(self.innerContainer,"file","Labels file: (Optional)", "labels_file");
+
+    self.innerContainer.append("button").attr({type: "submit",class: "btn btn-default"})
+      .on("click",self.submit)
+      .style("background","white")
+      .html("Upload Model");
+  };
+
+};
+
+PretrainedModel.mixins = {
+  select: function(obj,data,label,name){
+    var group = obj.append("div").attr("class","form-group");
+    group.append("label")
+        .attr("for",name).html(label);
+
+    var mySelect = group.append("select").attr({
+        class: "form-control",
+        name: name
+      });
+
+    mySelect.selectAll('option').data(data).enter()
+      .append("option")
+        .attr("value",function(d){return d.value})
+        .text(function(d){return d.text});
+
+    return mySelect;
+
+  },
+  field: function(obj,type,label,name){
+    var group = obj.append("div").attr("class","form-group");
+    group.append("label")
+        .attr("for",name).html(label);
+    return group.append("input")
+      .attr({type: type, class: "form-control", name: name});
+  }
+
+};
